d094f30c63eb5a12e637bfba85c0df384bf22fc7
Shiv Shankar Singh <75415783+sh1v-max@users.noreply.github.com>
2025-07-14 23:14:37 +0530

    simple practice

---
+++ practice.js
@@ -0,0 +1,12 @@
+const nums = [1, 2, 3];
+
+const result = nums.forEach((n) => {
+  return n * 2;
+});
+
+console.log(result);
+
+NaN === NaN      // false
+
+"fake it till you make it"  
+  // – JavaScript
